48736a5cfaa1861e6d354c00500a0bcba91a056e
FlorianGrollich <F-Grollich@web.de>
2025-02-13 13:00:33 +0100

    feat: add authpages

---
+++ frontend/src/features/Auth/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+const LoginPage: React.FC = () => {
+    return (
+        <div>
+            Login
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ frontend/src/features/Auth/pages/RegistrationPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const RegistrationPage: React.FC = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default RegistrationPage;
