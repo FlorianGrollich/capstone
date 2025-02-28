3d0ec4b2c5b8dec54b3a6bf64123817881182480
FlorianGrollich <F-Grollich@web.de>
2025-02-28 22:32:32 +0100

    feat: add config.py

---
+++ backend/app/core/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_DBNAME: str = "mydatabase"
+
+    class Config:
+        env_file = ".env"
