27786ad839a21c6638f0491eae2b2dccbbb97ef5
FlorianGrollich <F-Grollich@web.de>
2025-03-01 22:27:13 +0100

    feat: add collection

---
+++ backend/app/core/database.py
@@ -0,0 +1,9 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.core.config import settings
+
+client = AsyncIOMotorClient(settings.mongo_url)
+db = client.test
+
+def get_mongo_collection(collection_name: str):
+    return db[collection_name]
--- backend/app/core/depedencies.py
+++ backend/app/core/depedencies.py
@@ -1,4 +1,10 @@
+from app.core.database import get_mongo_collection
 from config import settings
+
 
 def get_settings():
     return settings
+
+
+def get_user_collection():
+    return get_mongo_collection("user")
