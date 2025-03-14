8598ee0bd86b802862c92464269f7e87328a4aa0
FlorianGrollich <F-Grollich@web.de>
2025-03-14 23:58:37 +0100

    feat: add loginformstate

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './features/Auth/authSlice';
+import authReducer from './features/Auth/slices/loginFormState.ts';
 
 const store = configureStore({
     reducer: {
