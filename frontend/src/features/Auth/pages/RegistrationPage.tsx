7e08519ecd23f7dc17da26b9df5710358c9c0917
FlorianGrollich <F-Grollich@web.de>
2025-03-19 16:03:16 +0100

    feat: finish register page

--- frontend/src/features/Auth/pages/RegistrationPage.tsx
+++ frontend/src/features/Auth/pages/RegistrationPage.tsx
@@ -1,10 +1,48 @@
 import * as React from "react";
+import TextField from "../../../components/TextField.tsx";
+import Button from "../../../components/Button.tsx";
+import {FcGoogle} from "react-icons/fc";
+import {useNavigate} from "react-router-dom";
+import {FaRegCircleCheck, FaRegCircleXmark} from "react-icons/fa6";
+import {IconContext} from "react-icons";
+
 
 const RegistrationPage: React.FC = () => {
-
+    const navigate = useNavigate();
     return (
-        <div className={"bg-primary"}>
-            Registration
+        <div className={"h-screen bg-gradient-to-tr from-primary to-accent flex items-center justify-center"}>
+            <div className="bg-white rounded-xl p-4 w-1/3">
+                <TextField onChange={() => {
+                }} label={"Email"}/>
+                <div className="h-4"></div>
+                <TextField onChange={() => {
+                }} label={"Password"}/>
+                <div className="h-4"></div>
+                <TextField onChange={() => {
+                }} label={"Repeat Password"}/>
+
+                <div className="h-4"></div>
+                <Button className={"w-full"}>Register</Button>
+                <div className={"h-2"}></div>
+                <p className={"text-xs text-gray-600"}>
+                    Already have an account?{" "}
+                    <button className={"hover:underline text-primary"} onClick={() => navigate("/login")}>Sign In
+                    </button>
+                </p>
+
+                <div className="relative my-4 w-full">
+                    <hr className="border-t-2 border-gray-300"/>
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <span className="bg-white px-2 text-gray-500">or</span>
+                    </div>
+                </div>
+
+                <button
+                    className={"px-4 py-2.5 rounded-lg font-medium transition duration-300 flex items-center justify-center bg-white border-2 border-gray-300 text-black w-full hover:border-primary  hover:shadow-lg"}>
+                    <FcGoogle/></button>
+
+
+            </div>
         </div>
     );
 };
