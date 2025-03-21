353fff3252d3c0677b9d5bf50a7b4ded30d229aa
FlorianGrollich <F-Grollich@web.de>
2025-03-21 21:07:13 +0100

    feat: add util methods for password hashing and verifications

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
-from motor.motor_asyncio import AsyncIOMotorCollection
 
 from app.core.depedencies import get_user_service
 from app.schemas.user import UserModel
@@ -9,7 +8,7 @@ from app.services.user_service import UserService
 router = APIRouter()
 
 
-@router.post("/create-user")
+@router.post("/register")
 async def create_user(
         user: UserModel,
         user_service: UserService = Depends(get_user_service)):
