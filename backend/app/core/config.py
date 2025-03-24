24403d14b75ef29ba6687ad8a0183a92e1381590
FlorianGrollich <F-Grollich@web.de>
2025-03-24 17:38:22 +0100

    feat: add jwt_expire_minutes and algorithm

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,20 @@
+from typing import ClassVar
+
 from dotenv import load_dotenv
 from passlib.context import CryptContext
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     app_name: str = "Wrong"
     mongo_url: str = "default url"
     jwt_secret_key: str = "default secret key"
+    jwt_expire_minutes: int = 30
+    jwt_algo: str = "HS256"
     model_config = SettingsConfigDict(env_file="../../.env")
-    pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    pwd_ctx: ClassVar[CryptContext] = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 settings = Settings()
