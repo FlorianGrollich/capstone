ba416405652614e91400a57a6019aae324b3cb73
FlorianGrollich <F-Grollich@web.de>
2025-03-24 17:38:42 +0100

    feat: add create jwt method

--- backend/app/utils/authentication.py
+++ backend/app/utils/authentication.py
@@ -1,4 +1,10 @@
+from datetime import datetime, timedelta
+
+import jwt
+
 from app.core.config import settings
+from app.schemas.token import TokenData
+from app.schemas.user import UserModel
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -7,3 +13,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return settings.pwd_ctx.hash(password)
+
+
+def create_jwt(user: UserModel):
+    to_encode = TokenData(email=user.email, )
+    current_time = datetime.now()
+    if settings.jwt_expire_minutes is not None:
+        expire = current_time + timedelta(minutes=settings.jwt_expire_minutes)
+    else:
+        expire = current_time + timedelta(minutes=15)
+    to_encode.exp = expire
+    encoded_jwt = jwt.encode(to_encode.model_dump(), settings.jwt_secret_key, algorithm=settings.jwt_algo)
+    return encoded_jwt
