a26d7278ae6b7d445b2e51ca71c922b5a938e34b
FlorianGrollich <F-Grollich@web.de>
2025-03-26 19:39:34 +0100

    fix: add provider to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,23 +5,24 @@ import LoginPage from "./features/Auth/pages/LoginPage.tsx";
 import RegistrationPage from "./features/Auth/pages/RegistrationPage.tsx";
 import ProjectMenu from "./features/Project/pages/ProjectMenu.tsx";
 import Layout from "./components/Layout.tsx";
+import {Provider} from "react-redux";
+import store from "./store.ts";
 
 const App: React.FC = () => {
 
     return (
-
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<LoginPage/>} />
-                <Route path="/register" element={<RegistrationPage/>} />
-                <Route element={<Layout/>}>
-                    <Route path="/" element={<ProjectMenu/>}/>
-                    <Route path="/project" element={<ProjectMenu/>}/>
-                </Route>
-            </Routes>
-
-        </BrowserRouter>);
-
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/register" element={<RegistrationPage/>}/>
+                    <Route element={<Layout/>}>
+                        <Route path="/" element={<ProjectMenu/>}/>
+                        <Route path="/project" element={<ProjectMenu/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>);
 };
 
 export default App;
