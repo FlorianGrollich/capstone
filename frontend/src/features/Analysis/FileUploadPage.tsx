d7c3ac1964be8c0c771525b03420fa95b06622bb
FlorianGrollich <F-Grollich@web.de>
2025-03-29 16:06:41 +0100

    fix: accepted file type in input

--- frontend/src/features/Analysis/FileUploadPage.tsx
+++ frontend/src/features/Analysis/FileUploadPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {useDispatch} from "react-redux";
 import {setFile} from "./slices/fileUploadSlice.ts";
+import Button from "../../components/Button.tsx";
 
 const FileUploadPage: React.FC = () => {
     const dispatch = useDispatch();
@@ -16,9 +17,13 @@ const FileUploadPage: React.FC = () => {
                 className={"w-full border-2 rounded-lg border-gray-400 border-dotted h-3/6 flex items-center justify-center"}>
                 <div className={"text-center text-gray-400"}>
                     <p>Upload Video Scene</p>
-                    <input type="file" onChange={handleFileChange}/>
+                    <input type="file" accept={"video/*"} onChange={handleFileChange}/>
                 </div>
             </div>
+            <Button
+                className={"mt-4"}
+                onClick={() => {
+                }}>Upload</Button>
         </div>
     );
 };
