a4719857def87927b29fbf398b8ffd8c3a35ec51
FlorianGrollich <F-Grollich@web.de>
2025-04-05 15:55:16 +0200

    fix: color

--- frontend/src/features/Project/CreateProjectButton.tsx
+++ frontend/src/features/Project/CreateProjectButton.tsx
@@ -3,10 +3,10 @@ import * as React from "react";
 const CreateProjectButton: React.FC = () => {
     return (
         <div className="group flex flex-col items-center justify-center border-gray-400 hover:border-primary hover:bg-gray-50 cursor-pointer border-dotted border-2 h-48 aspect-video rounded-lg transition-all duration-200">
-            <div className="text-4xl text-gray-400 group-hover:text-primary transition-colors">
+            <div className="text-4xl text-gray-400 group-hover:text-primary">
                 +
             </div>
-            <span className="text-sm text-gray-500 group-hover:text-primary mt-2 transition-colors">Create Project</span>
+            <span className="text-sm text-gray-400 group-hover:text-primary mt-2">Create Project</span>
         </div>
     );
 };
