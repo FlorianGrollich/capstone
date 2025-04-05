10e9c447eaf4f5c5e4f5b541d0c55f5ed7e06bec
FlorianGrollich <F-Grollich@web.de>
2025-04-05 15:57:42 +0200

    fix: remove transition

--- frontend/src/features/Project/CreateProjectButton.tsx
+++ frontend/src/features/Project/CreateProjectButton.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
+import {useNavigate} from "react-router-dom";
 
 const CreateProjectButton: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
-        <div className="group flex flex-col items-center justify-center border-gray-400 hover:border-primary hover:bg-gray-50 cursor-pointer border-dotted border-2 h-48 aspect-video rounded-lg transition-all duration-200">
+        <div onClick={() => navigate("/upload")}
+             className="group flex flex-col items-center justify-center border-gray-400 hover:border-primary hover:bg-gray-50 cursor-pointer border-dotted border-2 h-48 aspect-video rounded-lg transition-all duration-200">
             <div className="text-4xl text-gray-400 group-hover:text-primary">
                 +
             </div>
