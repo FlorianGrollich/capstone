3fc33cc8380a5c6696649ed17f07087b0521b2ae
FlorianGrollich <F-Grollich@web.de>
2025-04-05 16:54:15 +0200

    refactor:

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,15 +6,15 @@ from app.routes import user, project
 app = FastAPI()
 
 origins = [
-    "*",  # Common Vite/React development port
+    "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allows specific origins
-    allow_credentials=True,  # Allows cookies/authorization headers (important for auth)
-    allow_methods=["*"],  # Allows all standard HTTP methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(user.router)
-app.include_router(video.router)
+app.include_router(project.router)
