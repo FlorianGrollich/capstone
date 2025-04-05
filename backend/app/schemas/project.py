801824299b355f8637e227239b2c1a5edeb78894
FlorianGrollich <F-Grollich@web.de>
2025-04-05 16:21:05 +0200

    feat: add project basemodel

--- backend/app/schemas/project.py
+++ backend/app/schemas/project.py
@@ -1,6 +1,15 @@
+from enum import Enum
+
 from pydantic import BaseModel
+
+
+class ProjectState(Enum):
+    LOADING = "loading"
+    ERROR = "error"
+    FINISHED = "finished"
 
 
 class Project(BaseModel):
     video_url: str
     title: str
+    state: ProjectState
